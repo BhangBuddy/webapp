abd160dd7b69a0cbb302fe560aa752aefc9e1855
Witcher0766 <100211783+Witcher0766@users.noreply.github.com>
2023-07-26 19:46:48 +0530

    navbar-footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import {Home, Shop, About, Contact, Cart} from "./pages";
+import {Header, Footer} from "./components";
 
 function App() {
   return (
-    <div>
-    hello world
-    </div>
+   <>
+    <BrowserRouter>
+    <Header />
+    <Routes>
+    <Route path='/' element={<Home/>} />
+    <Route path='/shop' element={<Shop/>} />
+    <Route path='/about' element={<About/>} />
+    <Route path='/contact' element={<Contact/>} />
+    <Route path='/cart' element={<Cart/>} />
+    </Routes>
+
+    <Footer/>
+    </BrowserRouter>
+   </>
   );
 }
 
