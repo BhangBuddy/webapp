0c632337d647f7ff83d40b7ef759ec47057646a0
Saiyam Tuteja <saiyamtuteja@gmail.com>
2023-07-30 00:12:18 +0530

    Added the card api

---
+++ src/components/api/Cardapi.js
@@ -0,0 +1,86 @@
+const Cardapi = [
+    {
+      id: 1,
+      image: "webapp\src\assets\Beard Oil - Front - proc.jpg",
+      name: "Beard Oil",
+      category: "Men's",
+      price: "₹449",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    },
+  
+    {
+      id: 2,
+      image: "webapp\src\assets\Bhangola _ Hair Oil - proc.jpg",
+      name: "Hair Oil",
+      category: "Common",
+      price: "₹679",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+    },
+    {
+      id: 3,
+      image: "webapp\src\assets\Bhangola Face Serum Front 1 -proc.jpg",
+      name: "Face Serum",
+      category: "Common",
+      price: "₹619",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+    },
+    {
+      id: 4,
+      image: "webapp\src\assets\NoteBook.png",
+      name: "NoteBook",
+      category: "Common",
+      price: "₹349",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    },
+    {
+      id: 5,
+      image: "webapp\src\assets\Bhangola Face Wash - Front.png",
+      name: "Face Wash",
+      category: "Common",
+      price: "₹369",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+    },
+    {
+      id: 6,
+      image: "webapp\src\assets\shoes.JPG",
+      name: "Shoes",
+      category: "Common",
+      price: "₹1849",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+    },
+    {
+      id: 7,
+      image: "webapp\src\assets\storeCombo.png",
+      name: "Combo",
+      category: "common",
+      price: "₹---",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+    },
+    {
+      id: 8,
+      image: "webapp\src\assets\Cosmetics.png",
+      name: "Cosmetics",
+      category: "Common",
+      price: "₹---",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+    },
+    {
+      id: 9,
+      image: "webapp\src\assets\facecare_combo.jpg",
+      name: "facecare_combo",
+      category: "common",
+      price: "₹--",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit.  ",
+    },
+  ];
+  
+  export default Cardapi;
