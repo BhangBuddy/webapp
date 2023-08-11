d4c29987c4e57251f08a4b0f2d44eb036be429c9
Kartikey <codekartikey@gmail.com>
2023-08-11 20:31:15 +0530

    update style for cart

--- src/pages/cart/Cart.js
+++ src/pages/cart/Cart.js
@@ -22,23 +22,26 @@ const Cart = () => {
   };
   return (
     <>
-      <h1>Cart</h1>
-      <div className={styles["card-container"]}>
+      <div className={styles["cart-container"]}>
         {filteredCards.map((currElem) => {
           const { id, image, name, price, description } = currElem;
           const cartItem = cartItems.find(item => item.id === id);
 
           return (
-            <div className={styles["sub-container"]} key={id}>
+            <div className={styles["cart-sub"]} key={id}>
               <img src={image} alt={name} />
-              <div className={styles["card-data"]}>
-                <p><b>{name}</b></p>
-                <p><b>{price}</b></p>
+              <div className={styles["cart-carddata"]}>
+                <h1>{name}</h1>
+                <h2>{price}</h2>
+                <p>{description} </p>
                 {cartItem && (
                   <div className={styles["btn-div"]}>
-                    <button className={styles["pm-btn"]} onClick={() => handleAddToCart(id)}>+</button>
-                    <p>Quantity: {cartItem.quantity}</p>
-                    <button className={styles["pm-btn"]} onClick={() => handleRemoveFromCart(id)}>-</button>
+                    <h4>Quantity:</h4>
+                    <div className={styles["green-div"]}>
+                    <h3 className={styles["pm-btn"]} onClick={() => handleRemoveFromCart(id)}>{"<"}</h3>
+                    <p >{cartItem.quantity}</p>
+                    <h3 className={styles["pm-btn"]} onClick={() => handleAddToCart(id)}>{">"}</h3>
+                    </div>
                   </div>
                 )}
               </div>
@@ -46,8 +49,9 @@ const Cart = () => {
           );
         })}
       </div>
+        <button className={styles["buy-btn"]}> Buy Now </button>
     </>
-  );
-};
+    );
+  };
 
 export default Cart;
