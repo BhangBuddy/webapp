910434027b621ab67db752306a6541e4044a2dbc
Vishal Rawat <vr6994056@gmail.com>
2023-08-25 17:31:18 +0530

    Delete src directory

--- src/pages/index.js
+++
@@ -1,9 +0,0 @@
-export {default as Cart} from './cart/Cart';
-export {default as Contact} from './contact/Contact';
-export {default as Home} from './home/Home';
-export {default as orderHistory} from './orderHistory/OrderHistory';
-export {default as Login} from './auth/Login';
-export {default as Register} from './auth/Register';
-export {default as Reset} from './auth/Reset';
-export {default as Shop} from './shop/Shop';
-export {default as About} from './about/About';
--- src/pages/shop/Shop.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Shop = () => {
-  return (
-    <h1>Shop Now</h1>
-  )
-}
-
-export default Shop
